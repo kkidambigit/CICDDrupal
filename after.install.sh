b660f992e1ff296e4273b446b9fdf9094f238d7d
Arun Mulukalla <amulukalla@webmd.net>
2018-01-11 11:45:49 +0530

    pre demo test

--- after.install.sh
+++ after.install.sh
@@ -5,6 +5,7 @@ cd drupal/
 
 #Enable modules or set drush commands
 drush en show_node_titles -y
+drush en demo_list_view -y
 
 # Set the path for ://public stream.
 #drush vset --yes file_public_path "sites/default/files"
